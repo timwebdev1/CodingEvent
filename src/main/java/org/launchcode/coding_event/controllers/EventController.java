ed4000785c44a73a8f82b260fa37a7c75713f6cc
timwebdev1 <tlmcdermott79@gmail.com>
2024-10-09 10:43:32 -0500

    added form handler

--- src/main/java/org/launchcode/coding_event/controllers/EventController.java
+++ src/main/java/org/launchcode/coding_event/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.coding_event.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.Array;
 import java.util.ArrayList;
@@ -13,13 +15,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Code with Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -28,5 +27,12 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(){
         return "events/create";
+    }
+
+//    lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+    events.add(eventName);
+    return "redirect:/events";
     }
 }
