0c08219c1c05108096e5f140be3f4d869768bd7b
timwebdev1 <tlmcdermott79@gmail.com>
2024-10-14 07:50:17 -0500

    add description and id

--- src/main/java/org/launchcode/coding_event/controllers/EventController.java
+++ src/main/java/org/launchcode/coding_event/controllers/EventController.java
@@ -1,5 +1,7 @@
 package org.launchcode.coding_event.controllers;
 
+import org.launchcode.coding_event.data.EventData;
+import org.launchcode.coding_event.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,24 +17,33 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model){
-        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
 //  lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String renderCreateEventForm(Model model){
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
 //    lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-    events.add(eventName);
+    public String createEvent(@RequestParam String eventName,
+                              @RequestParam String eventDescription){
+    EventData.add(new Event(eventName, eventDescription));
     return "redirect:/events";
     }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model){
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
 }
